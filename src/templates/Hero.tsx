ba80c83f67a9cc299de1f5aca9ce483987ce25a4
dashmile <110690072+dashmile@users.noreply.github.com>
2022-08-19 16:06:55 -0700

    hero

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -12,13 +12,13 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
+          <Link href="">
+            <a></a>
           </Link>
         </li>
         <li>
           <Link href="/">
-            <a>Sign in</a>
+            <a></a>
           </Link>
         </li>
       </NavbarTwoColumns>
