12095b8025ffa94a04b4edc65677a9692d46bb66
dillytitan <80501424+dillytitan@users.noreply.github.com>
2022-08-20 13:34:41 -0700

    dark mode

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,13 @@
 function Contact() {
   return (
-    <div className="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="flex items-center min-h-screen">
       <div className="container mx-auto">
         <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
           <div className="text-center">
-            <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+            <h1 className="my-3 text-3xl font-semibold text-gray-700 ">
               Contact Us
             </h1>
-            <p className="text-gray-400 dark:text-gray-400">
+            <p className="text-gray-400">
               Fill up the form below to send us a message.
             </p>
           </div>
@@ -35,7 +35,7 @@ function Contact() {
               <div className="mb-6">
                 <label
                   htmlFor="name"
-                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  className="block mb-2 text-sm text-gray-600 "
                 >
                   Full Name
                 </label>
@@ -45,7 +45,7 @@ function Contact() {
                   id="name"
                   placeholder="Name"
                   required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300  dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
                 />
               </div>
               <div className="mb-6">
@@ -61,7 +61,7 @@ function Contact() {
                   id="email"
                   placeholder="you@company.com"
                   required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300  dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
                 />
               </div>
               <div className="mb-6">
@@ -77,7 +77,7 @@ function Contact() {
                   id="phone"
                   placeholder="+1 (555) 1234-567"
                   required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
                 />
               </div>
               <div className="mb-6">
@@ -93,7 +93,7 @@ function Contact() {
                   name="message"
                   id="message"
                   placeholder="Your Message"
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
                   required
                 ></textarea>
               </div>
